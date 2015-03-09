6d21b2abfbabe7be553305ee5440c31f43fdefeb
211211 <nhquan307@outlook.com>
2015-03-09 14:38:24 +0700

    Added coffeescripts files

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require('gulp');
-var gutil = require('gulp-util')
+var gutil = require('gulp-util');
+var coffee = require('gulp-coffee');
 gulp.task('log', function(){
     gutil.log('Workflows are awesome');
+});
+
+var coffeeSource = ['components/coffee/*.coffee']
+gulp.task('coffee', function(){
+    gulp.src(coffeeSource)
+        .pipe(coffee({ bare: true })
+            .on('error', gutil.log))
+        .pipe(gulp.dest('components/scripts'))
 });
---
+++ components/scripts/tagline.js
@@ -0,0 +1,7 @@
+var fill;
+
+(fill = function(item) {
+  return $('.tagline').append("" + item);
+})('the most creative minds in Art');
+
+fill;
