28cfb21790734a9125bc699a370ca78a752316ad
211211 <nhquan307@outlook.com>
2015-03-09 14:47:58 +0700

    concatenated javascript files

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,14 +1,26 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
+var concat = require('gulp-concat');
 gulp.task('log', function(){
     gutil.log('Workflows are awesome');
 });
 
-var coffeeSource = ['components/coffee/*.coffee']
+var coffeeSources = ['components/coffee/*.coffee']
+var jsSources = [
+    'components/scripts/tagline.js',
+    'components/scripts/pixgrid.js',
+    'components/scripts/template.js'
+];
 gulp.task('coffee', function(){
-    gulp.src(coffeeSource)
+    gulp.src(coffeeSources)
         .pipe(coffee({ bare: true })
             .on('error', gutil.log))
         .pipe(gulp.dest('components/scripts'))
+});
+
+gulp.task('js', function(){
+    gulp.src(jsSources)
+        .pipe(concat('scripts.js'))
+        .pipe(gulp.dest('builds/development/js'))
 });
